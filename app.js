2419773d4118c1c10eaf936debba47913e72ad11
Phil Gengler <phil@pgengler.net>
2014-04-11 10:59:32 -0400

    Items can have tags

--- app.js
+++ app.js
@@ -12,11 +12,18 @@ Ember.Handlebars.helper('formatDate', function(value) {
 
 App.Router.map(function() {
 	this.route('items');
+	this.route('tags');
 });
 
 App.ItemsRoute = Ember.Route.extend({
 	model: function() {
 		return this.store.find('item');
+	}
+});
+
+App.TagsRoute = Ember.Route.extend({
+	model: function() {
+		return this.store.find('tag');
 	}
 });
 
@@ -62,17 +69,30 @@ App.Item = DS.Model.extend({
 			classes.push('future');
 		}
 		return classes.join(' ');
-	}.property('done', 'date')
+	}.property('done', 'date'),
+
+	tags: DS.hasMany('tag', { async: true })
+});
+
+App.Tag = DS.Model.extend({
+	name: DS.attr('string'),
+	items: DS.hasMany('item', { async: true })
 });
 
 App.Item.FIXTURES = [
-	{ id: 1, done: false, date: new Date(), event: "Rewrite todolist UI with Ember", location: null, startTime: null, endTime: null },
-	{ id: 2, done: true, date: new Date(), event: "Create some fixture data", location: null, startTime: null, endTime: null },
-	{ id: 3, done: false, date: new Date(), event: "With a start time only", location: null, startTime: '0400', endTime: null },
-	{ id: 4, done: false, date: new Date(), event: "With an end time only", location: null, startTime: null, endTime: '1600' },
-	{ id: 5, done: false, date: new Date(), event: "With start and times", location: null, startTime: '0400', endTime: '1600' },
-	{ id: 6, done: false, date: null, event: "Item without a date", location: null, startTime: null, endTime: null },
-	{ id: 7, done: false, date: new Date("2014-04-07T23:59:59"), event: "Item with an older date", location: null, startTime: null, endTime: null }
+	{ id: 1, done: false, date: new Date(), event: "Rewrite todolist UI with Ember", location: null, startTime: null, endTime: null, tags: [ 1, 2 ] },
+	{ id: 2, done: true, date: new Date(), event: "Create some fixture data", location: null, startTime: null, endTime: null, tags: [ ] },
+	{ id: 3, done: false, date: new Date(), event: "With a start time only", location: null, startTime: '0400', endTime: null, tags: [ ] },
+	{ id: 4, done: false, date: new Date(), event: "With an end time only", location: null, startTime: null, endTime: '1600', tags: [ ] },
+	{ id: 5, done: false, date: new Date(), event: "With start and times", location: null, startTime: '0400', endTime: '1600', tags: [ ] },
+	{ id: 6, done: false, date: null, event: "Item without a date", location: null, startTime: null, endTime: null, tags: [ ] },
+	{ id: 7, done: false, date: new Date("2014-04-07T23:59:59"), event: "Item with an older date", location: null, startTime: null, endTime: null, tags: [ ] }
+];
+
+App.Tag.FIXTURES = [
+	{ id: 1, name: 'first tag', items: [ 1 ] },
+	{ id: 2, name: 'second tag' },
+	{ id: 3, name: 'third tag' }
 ];
 
 function datesEqual(a, b)
