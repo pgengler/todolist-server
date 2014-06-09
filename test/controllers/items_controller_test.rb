ebad516391b66fa9fe08f24ab30ef1523e669f19
Phil Gengler <philip.gengler@nytimes.com>
2014-06-09 11:52:06 -0400

    Remove debugging code from a test and fix it so that it actually passes

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -8,10 +8,8 @@ class ItemsControllerTest < ActionController::TestCase
 
   test "index view includes appropriate items" do
     get :index
-    items = JSON.parse(response.body)
-    p items
-    p response.body
-    assert_equal 2, items.length
+    body = JSON.parse(response.body)
+    assert_equal 2, body['items'].length
   end
 
 end
