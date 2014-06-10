e309084abb8337d406b9f286fb9e4e48adae34c2
Phil Gengler <philip.gengler@nytimes.com>
2014-06-10 13:09:51 -0400

    Add 'create' action for items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,15 @@
 class ItemsController < ApplicationController
-  def index
-    render json: Item.all
-  end
+    def index
+        render json: Item.all
+    end
+
+    def create
+        Item.create! item_params
+    end
+
+    private
+
+    def item_params
+        params.required(:item).permit(:event, :location)
+    end
 end
--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -1,15 +1,20 @@
 require 'test_helper'
 
 class ItemsControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-  end
+    test "should get index" do
+        get :index
+        assert_response :success
+    end
 
-  test "index view includes appropriate items" do
-    get :index
-    body = JSON.parse(response.body)
-    assert_equal 2, body['items'].length
-  end
+     test "index view includes appropriate items" do
+        get :index
+        body = JSON.parse(response.body)
+        assert_equal 2, body['items'].length
+    end
 
+    test "can create new items" do
+        assert_difference 'Item.count' do
+            post :create, item: { event: "New event", location: "New Location" }
+        end
+    end
 end
