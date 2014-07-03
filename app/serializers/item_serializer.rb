8525e7bdb629ee75fd712cf2cc53944813931b7e
Phil Gengler <phil@pgengler.net>
2014-07-03 17:17:53 -0400

    Serialize dates with a 23:59:59.999 time component

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,3 +1,11 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :date, :event, :location, :start, :end, :done
+	attributes :id, :date, :event, :location, :start, :end, :done
+
+	def date
+		if object.date
+			"#{object.date}T23:59:99.999Z"
+		else
+			nil
+		end
+	end
 end
