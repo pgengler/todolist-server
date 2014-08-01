8332b58663717bdf8288b1de53a6cca6311072c9
Phil Gengler <phil@pgengler.net>
2014-08-01 18:20:42 -0400

    Make automatic 'position' setting for an ItemTag happen on the model instead of in the controller

--- app/controllers/item_tags_controller.rb
+++ app/controllers/item_tags_controller.rb
@@ -12,19 +12,8 @@ class ItemTagsController < ApplicationController
 		@item = Item.find(params[:item_tag][:item_id])
 		@tag = Tag.find(params[:item_tag][:tag_id])
 
-		@item_tag = ItemTag.create!(item: @item, tag: @tag, position: next_position(@item, @tag))
+		@item_tag = ItemTag.create!(item: @item, tag: @tag)
 
 		render json: @item_tag, status: :created
-	end
-
-	private
-
-	def next_position(item, tag)
-		last_item_tag = ItemTag.where(item_id: item.id, tag_id: tag.id).order('position DESC').first
-		if last_item_tag
-			last_item_tag.position + 1
-		else
-			1
-		end
 	end
 end
--- app/models/item_tag.rb
+++ app/models/item_tag.rb
@@ -2,6 +2,19 @@ class ItemTag < ActiveRecord::Base
 	belongs_to :item
 	belongs_to :tag
 
-	validates :position, presence: true
 	validates :position, uniqueness: { scope: :tag_id }
+
+	before_save :set_position
+
+	private
+
+	def set_position
+		return if self.position
+		last_item_tag = ItemTag.where(item_id: item.id, tag_id: tag.id).order('position desc').first
+		if last_item_tag
+			self.position = last_item_tag.position + 1
+		else
+			self.position = 1
+		end
+	end
 end
