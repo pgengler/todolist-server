7343680f2472f4b6a0c62457a64beab7db829c02
Phil Gengler <phil@pgengler.net>
2014-10-03 15:42:27 -0400

    When creating a new Day, automatically create Tasks for each RecurringTask for that day of the week

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,18 +1,26 @@
 class Day < ActiveRecord::Base
-  has_many :tasks
+	after_create :populate_recurring_tasks
+	has_many :tasks
 
-  def self.sliding_window
-    days = [ ]
-    date = 1.day.ago
-    5.times do
-      day = where(date: date).first
-      unless day
-        day = Day.create!(date: date)
-      end
-      days << day
-      date = date + 1.day
-    end
+	def self.sliding_window
+		days = [ ]
+		date = 1.day.ago
+		5.times do
+			day = where(date: date).first
+			unless day
+				day = Day.create!(date: date)
+			end
+			days << day
+			date = date + 1.day
+		end
 
-    days
-  end
+		days
+	end
+
+	private
+	def populate_recurring_tasks
+		RecurringTask.where(day: date.wday).each do |recurring_task|
+			tasks.create(description: recurring_task.description)
+		end
+	end
 end
