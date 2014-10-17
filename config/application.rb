4b5d2ae6162119181ebdca74dde882a9fcb08e4a
Phil Gengler <phil@pgengler.net>
2014-10-17 15:32:23 -0400

    Add and configure Rack::Cors

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,12 @@ module Todo
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.middleware.insert_before "ActionDispatch::Static", "Rack::Cors" do
+      allow do
+        origins '*.pgengler.net'
+        resource '*', :headers => :any, :methods => [:get, :post, :delete, :put, :options, :head]
+      end
+    end
   end
 end
