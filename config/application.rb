81a8fdb7c5a00a48d273e38594992e562528e8f4
Phil Gengler <phil@pgengler.net>
2020-10-10 09:39:44 -0400

    Include action_mailer/railtie because of an issue with Devise

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require "active_job/railtie"
 require "active_record/railtie"
 # require "active_storage/engine"
 require "action_controller/railtie"
-# require "action_mailer/railtie"
+require "action_mailer/railtie" # need this because of an issue with Devise (https://github.com/heartcombo/devise/issues/5140)
 # require "action_mailbox/engine"
 # require "action_text/engine"
 require "action_view/railtie"
