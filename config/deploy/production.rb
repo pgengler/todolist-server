beca22a5f5aae66c871bd16c0834cdef5184ceb6
Phil Gengler <phil@pgengler.net>
2020-10-10 07:42:12 -0400

    Match Ruby version in config/deploy/production.rb to desired version

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 server 'hyperion.pgengler.net', user: 'todolist', roles: %w{web app db}
 set :rails_env, :production
-set :rvm_ruby_version, 'ruby-2.4.0@todolist'
+set :rvm_ruby_version, 'ruby-2.6.5@todolist'
